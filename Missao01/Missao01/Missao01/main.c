7991b4e92eb8c4b81c9a3fb3147bbf6df6140a00
Raphael Ponce <raphael.fleury@b2wdigital.com>
2020-05-05 21:26:19 -0300

    Missao0

---
+++ Missao01/Missao01/Missao01/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void) {
+    /* Operações aritimetricas basicas */
+//    int numero1, numero2, diferenca;
+//
+//    printf("Digite o primeiro numero inteiro: ");
+//    scanf("%d", &numero1);
+//    printf("Digite o segundo numero inteiro: ");
+//    scanf("%d", &numero2);
+//
+//    diferenca = numero1 - numero2;
+//    printf("O resultado da diferenca = %d", diferenca);
+    
+    /* Converter temperatura de Celsius em Fahrenheit */
+//    float cels, fahr;
+//
+//    printf("Digite a temperatura em Celsius ");
+//    scanf("%f", &cels);
+//
+//    fahr = 1.8 * cels + 32;
+//
+//    printf("Temperatura em Fahrenheit: %f", fahr);
+    
+    /* Converter segundos em horas e minutos */
+//    int total, hora, minutos, segundos;
+//
+//    printf("Digite o total em segundos: ");
+//    scanf("%d", &total);
+//
+//    hora = total / 3600;
+//    minutos = (total % 3600) / 60;
+//    segundos = total % 60;
+//
+//    printf("%d horas %d minutos e %d segundos", hora, minutos, segundos);
+    
+    
+    
+    
+    
+    return 0;
+}
